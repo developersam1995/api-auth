5d6ef4bea0667fe84915b94807c5b0fc71497bb5
sam1995 <samrudh.kumar@mountblue.io>
2018-06-30 11:09:58 +0530

    8 finished

--- controllers/users.js
+++ controllers/users.js
@@ -32,6 +32,8 @@ module.exports = {
 
   signIn: async (req, res, next) => {
     // generate token
+    const token = signToken(req.user);
+    res.status(200).json({ token });  
   },
 
   secret: async (req, res, next) => {
--- passport.js
+++ passport.js
@@ -8,14 +8,14 @@ const LocalStrategy = require('passport-local').Strategy;
 passport.use(new JwtStrategy({
   jwtFromRequest: ExtractJwt.fromHeader('authorization'),
   secretOrKey: 'iamsam'
-}, async (payload,done)=>{
-  try{
+}, async (payload, done) => {
+  try {
     //find the user specified in token
     const user = await User.findById(payload.subject);
-    
+
     //if user doesnt exist handle
-    if(!user) {
-      return done(null,false);
+    if (!user) {
+      return done(null, false);
     }
 
     //otherwise return user
@@ -29,18 +29,29 @@ passport.use(new JwtStrategy({
 
 passport.use(new LocalStrategy({
   usernameField: 'email'
-}, async (email, password, done) =>{
-  //find user given email
-  const user = await User.findOne({email});
+}, async (email, password, done) => {
+  try {
+    //find user given email
+    const user = await User.findOne({ email });
 
-  //  if not handle
-  if(!user) {
-    return done(unll, false);
+    //  if not handle
+    if (!user) {
+      return done(unll, false);
+    }
+
+    // check if pswd is correct
+    const isMatch = await user.isValidPassword(password);
+
+    //ifnot
+    if (!isMatch) {
+      return done(null, false);
+    }
+
+    //return user)
+    done(null, user);
+  } catch (error) {
+    done(error, false);
   }
 
-  // check if pswd is correct
- 
-  //ifnot
 
-  //return user
 }));
